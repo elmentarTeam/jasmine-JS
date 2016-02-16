dbf5d94397e6588af2361b55acf6c4e0e67515af
Kalantar <Calantarov@gmail.com>
2016-02-16 15:39:35 +0200

    init the project

---
+++ spec/myNameSpec.js
@@ -0,0 +1,5 @@
+describe("My Name Is", function(){
+    it('shoud return my name', function(){
+        expect (myNameIs()).toEqual("Nick")
+    });
+});
---
+++ src/myName.js
@@ -0,0 +1,3 @@
+var myNameIs = function(){
+    return("Nick")
+};
